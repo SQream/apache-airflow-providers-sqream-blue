7b2db6da59275f1e072c9066b49b11afca7c42cd
danielg <danielg@sqreamtech.com>
2023-04-18 19:01:38 +0300

    upgrade

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ setup(
     description='About Apache Airflow - A platform to programmatically author, schedule, and monitor workflows.',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Apache Airflow",
+        "Framework :: Apache Airflow :: Provider",
+    ],
+    python_requires='>=3.9',
     entry_points={
         "apache_airflow_provider": [
             "provider_info=sqream_blue.__init__:get_provider_info"
@@ -19,14 +27,9 @@ setup(
     },
     license='Apache License 2.0',
     packages=['sqream_blue', 'sqream_blue.hooks','sqream_blue.operators'],
-    install_requires=['apache-airflow>=2.0', 'apache-airflow-providers-common-sql==1.3.2', 'pysqream-blue==1.0.26'],
+    install_requires=['pysqream-blue>=1.0.26', 'apache-airflow>=2.0', 'apache-airflow-providers-common-sql==1.3.2'],
     setup_requires=['setuptools', 'wheel'],
     author='SQream',
     author_email='info@sqream.com',
     url='https://github.com/SQream/apache-airflow-providers-sqream-blue',
-    classifiers=[
-        "Framework :: Apache Airflow",
-        "Framework :: Apache Airflow :: Provider",
-    ],
-    python_requires='>=3.9',
 )
